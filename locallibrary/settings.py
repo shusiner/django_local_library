6c319b8542ec63606fed43b64ec13d572f96f2f3
shusiner <quantum.plato@gmail.com>
2018-08-02 03:33:48 +0800

    Update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wiw3p^ujh%pw5q39-qtbfcx(g-=e^)q@gto0arsr1y*z%2$f10'
+#SECRET_KEY = 'wiw3p^ujh%pw5q39-qtbfcx(g-=e^)q@gto0arsr1y*z%2$f10'
+
+SECRET_KEY = '1'
 
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'wiw3p^ujh%pw5q39-qtbfcx(g-=e^)q@gto0arsr1y*z%2$f10')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
