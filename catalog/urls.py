8525e7573b7376764c88d214332d53c35acfc9d8
shusiner <quantum.plato@gmail.com>
2018-08-02 01:13:15 +0800

    First version of application moved into github

---
+++ catalog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('borrowedbooks/', views.LibrarianListView.as_view(), name='all-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('authors/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('authors/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('authors/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+]
